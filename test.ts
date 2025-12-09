f170f4e8ff60c8f8821d6f59e425909ae8318048
alrightCha <ychamam7@gmail.com>
2025-12-09 07:04:54 +0100

    added benchmark reference test

---
+++ test.ts
@@ -0,0 +1,47 @@
+import { VECTOR } from "./testvec.ts";
+import { hash } from "./js/blake3.ts";
+import { assertStrictEquals } from "https://deno.land/std/assert/mod.ts";
+
+function buf2hex(buffer: ArrayLike<number>) {
+  return [...new Uint8Array(buffer)]
+    .map((x) => x.toString(16).padStart(2, "0"))
+    .join("");
+}
+
+Deno.test({
+  name: "TEST VECTOR",
+  fn() {
+    const buffer = new Uint8Array(128 * 1024);
+    for (let i = 0; i < buffer.length; ++i) {
+      buffer[i] = i & 0xff;
+    }
+
+    for (let id = 0; id < VECTOR.length; ++id) {
+      const [size, expectedHash] = VECTOR[id]! as [number, string];
+      const input = new Uint8Array(buffer.buffer, 0, size);
+      const actualHash = buf2hex(hash(input));
+      // if (actualHash !== expectedHash) {
+      //   console.log(size, size - previousPow2(size));
+      // }
+      assertStrictEquals(actualHash, expectedHash, `size=${size};` + input);
+    }
+  },
+});
+
+Deno.test({
+  name: "Right Rotate",
+  fn() {
+    const input = new Uint8Array(32);
+    // this will force the first call to rightRotate to have input=0xffffffff or -1 but
+    // regardless it should work.
+    input[0] = 25;
+    input[1] = 199;
+    input[2] = 231;
+    input[3] = 68;
+    const actualHash = buf2hex(hash(input));
+    assertStrictEquals(
+      actualHash,
+      "a1d06679e5df29d2b07da0e86a97ede72f9aed3dd186631c0adcc1d46788bcf4",
+    );
+  },
+});
